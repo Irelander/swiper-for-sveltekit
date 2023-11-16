8f3dd44435dd8df17b9aac4908990453127c94ed
Irelander <iiitttiii@naver.com>
2023-11-16 13:16:17 +0900

    Fix: json string to parse

--- src/core/breakpoints/setBreakpoint.mjs
+++ src/core/breakpoints/setBreakpoint.mjs
@@ -7,7 +7,13 @@ const isGridEnabled = (swiper, params) => {
 export default function setBreakpoint() {
   const swiper = this;
   const { realIndex, initialized, params, el } = swiper;
-  const breakpoints = params.breakpoints;
+  let breakpoints = params.breakpoints;
+
+  // If breakponits serialized object parse it
+  try {
+    if (typeof breakpoints === 'string') breakpoints = JSON.parse(breakpoints);
+  } catch (e) {}
+
   if (!breakpoints || (breakpoints && Object.keys(breakpoints).length === 0)) return;
 
   // Get breakpoint for window width and update parameters
